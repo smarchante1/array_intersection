91cd4c8eb1d5b328173189b9c49a342f948ca717
Stephanie Marchante <smarcha04@gmail.com>
2019-04-16 22:43:22 -0700

    implemented array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,20 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n * m)
+# Space complexity: O(n)
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil 
+    return []
+  end
+
+  result = []
+
+  array1.each do |i|
+    array2.each do |j|
+      if i == j
+        result << j
+      end
+    end
+  end
+
+  return result
 end
